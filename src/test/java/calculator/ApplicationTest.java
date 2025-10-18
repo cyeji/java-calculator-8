41f20966fed4fba9cfb555b86c0c138105d098a0
cyeji <98408267+cyeji@users.noreply.github.com>
2025-10-18 20:31:43 +0900

    test : 계산기 테스트케이스 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -8,7 +9,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
+    @DisplayName("커스텀 구분자 - 리터럴 \\n 형태도 인식")
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
@@ -17,15 +20,71 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    @DisplayName("음수 입력 시 예외")
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    @DisplayName("허용되지 않은 문자 포함 시 예외")
+    void 허용되지않은_문자_사용() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("_1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 - 커스텀 구분자 여러개 등록 시 인식")
+    void 커스텀_구분자_다수_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    @DisplayName("커스텀 파이프(|) 구분자 - 리터럴 \\n")
+    void 커스텀_파이프_리터럴n() {
+        assertSimpleTest(() -> {
+            run("//|\\n10|20|30");
+            assertThat(output()).contains("결과 : 60");
+        });
+    }
+
+    @Test
+    @DisplayName("기본 구분자(쉼표, 콜론) 혼용")
+    void 더하기연산_결과_성공() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
+    @DisplayName("실수 연산 불가")
+    void 실수_연산_에러() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1.1231,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("커스텀 연산자 외 다른 연산자 입력 가능")
+    void custom_() {
+        assertSimpleTest(() -> {
+            run("//|\\n10|20,30");
+            assertThat(output()).contains("결과 : 60");
+        });
     }
 
     @Override
     public void runMain() {
         Application.main(new String[]{});
     }
+
 }
